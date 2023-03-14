3c9df746d24c48c9cafed924f19c10b092e72e2b
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2023-03-14 15:22:05 +0200

    Use 1-4 for minute part in file name

--- src/main/kotlin/fi/hsl/transitlog/ApcArchiveFile.kt
+++ src/main/kotlin/fi/hsl/transitlog/ApcArchiveFile.kt
@@ -27,7 +27,7 @@ class ApcArchiveFile(val path: Path, private val contentDuration: Duration) : Au
             val receivedAtLocalTime = Instant.ofEpochMilli(timestamp).atZone(ZoneId.of("Europe/Helsinki")).toLocalDateTime()
 
             val dateHour = receivedAtLocalTime.truncatedTo(ChronoUnit.HOURS)
-            val minute = receivedAtLocalTime.minute / 15
+            val minute = (receivedAtLocalTime.minute / 15) + 1
 
             return "apc_${DATE_HOUR_FORMATTER.format(dateHour)}-$minute.parquet"
         }
